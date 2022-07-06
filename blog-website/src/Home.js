efeb61e90bd0614f10d7abbf3010c92944739f57
sgk2303 <gkvv2303@gmail.com>
2022-07-06 10:27:45 +0530

    first commit

---
+++ blog-website/src/Home.js
@@ -0,0 +1,18 @@
+function Home(){
+    return(
+        <div className='home-container'>
+            <div className='home-content'>
+                <div className='home-content-header'>
+                    <h1>Welcome to SGK Blog</h1>
+                </div>
+                <div className='home-content-desc'>
+                    <p>
+                        This is a simple blog application that I have created to learn React.    
+                    </p>    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
